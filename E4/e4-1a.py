64951bf8f478da830785bf1b115983eaeee2a68f
RodrigoOkido <rodrigookido@gmail.com>
2018-09-29 18:26:44 -0300

    Updated upgma method

--- E4/e4-1a.py
+++ E4/e4-1a.py
@@ -1,23 +1,45 @@
 
 #Gorila, Orangotango, Humano, Chimpanze, Gibao
-upgma_matrix = [[0, 0.1890, 0.1100, 0.1130, 0.2150],
-[0.1890, 0, 0.1790, 0.1920, 0.2110],
-[0.1100, 0.1790, 0, 0.0940, 0.2050],
-[0.1130, 0.1920, 0.09405, 0, 0.2140],
-[0.2150, 0.2110, 0.2050, 0.2140, 0]]
+#GORILA = 0 , ORANGOTANGO = 1, HUMANO = 2, CHIMPANZE = 3, GIBAO = 4
+upgma_matrix = [
+[0.1890],
+[0.1100, 0.1790],
+[0.1130, 0.1920, 0.09405],
+[0.2150, 0.2110, 0.2050, 0.2140]]
 
+matrix_temp = 0
+result = {}
+list = []
 
 #Executes the UPGMA Method
 def upgma_method ():
     #Slowest value start with high number for initialization purpose
     slowest_value = 100000
+    row = 0
+    column = 0
 
     #Search for the lowest number comparison between species
     for i in range(len(upgma_matrix)):
         for j in range(len(upgma_matrix[i])):
             if upgma_matrix[i][j] < slowest_value and upgma_matrix[i][j] != 0:
                 slowest_value = upgma_matrix[i][j]
-    print slowest_value
+                row = i
+                column = j
+
+    distance = slowest_value / 2
+    cluster = "%d - %d" % (row , column)
+    #Put the distance of the cluster on final result
+    result[cluster] = distance
+    print result
+    #Values - GOR: Gorila , OR: Orangotango, H: Humano, CHI: Chimpanze, GI: Gibao
+    for i in range(len(upgma_matrix[i])):
+        if i != row and i != column:
+            dist = (upgma_matrix[i][column]+upgma_matrix[i][row])/2
+            list.append(dist);
+
+
+
+    #Calculate the new distance matrix after clustering the slowest values
 
 
 
---
+++ E4/e4-1a2.py
@@ -0,0 +1,93 @@
+distance = {}
+
+#Executes the UPGMA Method
+def upgma_method(matrix, assoc_label):
+    # Keep running until have no more element in table
+    while len(assoc_label) > 1:
+        # Locate lowest cell in the table
+        x, y = find_lowest_distance(matrix)
+
+        # Update matrix after finding the lowest distance (cluster)
+        update_matrix(matrix, assoc_label, x, y)
+
+        # Update the label
+        join_labels(assoc_label, x, y)
+
+    # Return the final label
+    return assoc_label[0]
+
+
+# Locates the cell with the lowest distance in the matrix
+def find_lowest_distance(matrix):
+    # initialize with high number for initialization purpose
+    lowest_distance = 1000000000
+
+    # index coordinates
+    x = 0
+    y = 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] < lowest_distance:
+                lowest_distance = matrix[i][j]
+                x, y = i, j
+
+    # Return coordinates
+    return x, y
+
+
+# Update matrix after finding the lowest distance (cluster)
+def update_matrix(matrix, labels, a, b):
+    # Swap if the indices are not ordered
+    if b < a:
+        a, b = b, a
+
+    # For the lower index, reconstruct the entire row (A, i), where i < A
+    row = []
+    for i in range(0, a):
+        row.append((matrix[a][i] + matrix[b][i])/2)
+    matrix[a] = row
+
+    # Then, reconstruct the entire column (i, A), where i > A
+    for i in range(a+1, b):
+        matrix[i][a] = (matrix[i][a]+matrix[b][i])/2
+
+
+    # We get the rest of the values from row i
+    for i in range(b+1, len(matrix)):
+        matrix[i][a] = (matrix[i][a]+matrix[i][b])/2
+        # Remove the (now redundant) second index column entry
+        del matrix[i][b]
+
+    # Remove the (now redundant) second index row
+    del matrix[b]
+
+
+# Function to join two label (cluster)
+def join_labels(labels, a, b):
+    # Swap if the indices are not ordered
+    if b < a:
+        a, b = b, a
+
+    # Join the labels in the first index
+    labels[a] = "[" + labels[a] + " - " + labels[b] + "]"
+    del labels[b]
+
+
+
+def main():
+    # Association of each element of the matrix.
+    assoc_label = ["GOR", "ORANG", "HUM", "CHI", "GIB"]
+    upgma_matrix = [
+        [],
+        [0.1890],
+        [0.1100, 0.1790],
+        [0.1130, 0.1920, 0.09405],
+        [0.2150, 0.2110, 0.2050, 0.2140]
+        ]
+
+    print upgma_method(upgma_matrix, assoc_label)
+    print distance
+
+if __name__ == "__main__":
+    main()
