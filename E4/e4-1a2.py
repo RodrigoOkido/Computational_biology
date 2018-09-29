89618c0e3aa43897c1bb595e6fda7b8baea97f55
RodrigoOkido <rodrigookido@gmail.com>
2018-09-29 19:51:57 -0300

    Updating readability of the code

--- E4/e4-1a2.py
+++ E4/e4-1a2.py
@@ -2,16 +2,16 @@ dist_specie = {}
 
 #Executes the UPGMA Method
 def upgma_method(matrix, assoc_label):
-    # Keep running until have no more element in table
+    # Keep running until have no more element in matrix
     while len(assoc_label) > 1:
-        # Locate lowest cell in the table
+        # Locate the coordinates with the lowest number in the matrix
         x, y = find_lowest_distance(matrix, assoc_label)
 
         # Update matrix after finding the lowest distance (cluster)
         update_matrix(matrix, x, y)
 
         # Update the label
-        join_labels(assoc_label, x, y)
+        join_label(assoc_label, x, y)
 
     # Return the final label
     return assoc_label[0]
@@ -19,9 +19,9 @@ def upgma_method(matrix, assoc_label):
 
 # Locates the cell with the lowest distance in the matrix
 def find_lowest_distance(matrix, labels):
+
     # initialize with high number for initialization purpose
     lowest_distance = 1000000000
-
     # index coordinates
     x = 0
     y = 0
@@ -46,18 +46,17 @@ def update_matrix(matrix, a, b):
     if b < a:
         a, b = b, a
 
-    # For the lower index, reconstruct the entire row (A, i), where i < A
+    # Updating row (A, i), where i < A
     row = []
     for i in range(0, a):
         row.append((matrix[a][i] + matrix[b][i])/2)
     matrix[a] = row
 
-    # Then, reconstruct the entire column (i, A), where i > A
+    # Update entire column (i, A), where i > A
     for i in range(a+1, b):
         matrix[i][a] = (matrix[i][a]+matrix[b][i])/2
 
-
-    # We get the rest of the values from row i
+    # Rest values from row i
     for i in range(b+1, len(matrix)):
         matrix[i][a] = (matrix[i][a]+matrix[i][b])/2
         # Remove the column
@@ -68,7 +67,7 @@ def update_matrix(matrix, a, b):
 
 
 # Function to join two label (cluster)
-def join_labels(labels, a, b):
+def join_label(labels, a, b):
     # Swap if the indices are not ordered
     if b < a:
         a, b = b, a
@@ -82,6 +81,9 @@ def join_labels(labels, a, b):
 def main():
     # Association of each element of the matrix.
     assoc_label = ["GOR", "ORANG", "HUM", "CHI", "GIB"]
+    
+    # Building the matrix. This will be a triangular matrix due to the fact that
+    #the other half is symmetric and the main diagonal filled with zeros.
     upgma_matrix = [
         [],
         [0.1890],
