02171a7bec9cc1103d4aa398b09f48285d37461c
RodrigoOkido <rodrigookido@gmail.com>
2018-10-02 01:15:39 -0300

    Some improvements and code documentation fixes.

--- E5/e5-1a.py
+++ E5/e5-1a.py
@@ -15,11 +15,10 @@ node_counter = 0
 # Takes the lowest pair distance species and return their coordinates.
 # (Step 1 and Step 2 are made in this function)
 def calculate_distance(matrix, labels):
-    # initialize with high number for initialization purpose
+    # Initialize with high number for initialization purpose.
     lowest_pair_dist = 1000000000
-    pair_distances = []
 
-    # index coordinates
+    # Index coordinates
     x = 0
     y = 0
 
@@ -33,13 +32,13 @@ def calculate_distance(matrix, labels):
         result_specie = dist/(len(matrix[i]) - 2)
         s_dist.append(result_specie)
 
-    # Take the species with the lowest distance (Step 2)
+    # Take the pair of species with the lowest distance (Step 2)
+    # If equals distances, choses the first
     for i in range(len(matrix)):
         dist = 0
         for j in range(len(labels)):
             if i != j:
                 dist = matrix[i][j] - s_dist[i] - s_dist[j]
-                pair_distances.append(dist)
                 if dist < lowest_pair_dist:
                     lowest_pair_dist = dist
                     x, y = i, j
@@ -57,11 +56,11 @@ def calculate_distance(matrix, labels):
 def create_node_and_update_matrix (matrix, labels, x, y):
     global node_counter
 
+    # Calculate the distance of the especie to the node U. (Step 3)
     s_xU = matrix[x][y]/2 + ((s_dist[x] - s_dist[y])/2)
     s_yU = matrix[x][y]/2 + ((s_dist[y] - s_dist[x])/2)
 
-    new_dist = 0
-
+    # (Steps 4 and 5)
     # Update entire column (i, x), where i > x.
     for i in range(x+1, y):
         matrix[i][x] = (matrix[i][x] + matrix[y][i] - matrix[x-1][y])/2
@@ -81,7 +80,6 @@ def create_node_and_update_matrix (matrix, labels, x, y):
     dist_calculated_nodes.append(labels[x])
     del labels[y]
 
-
     del s_dist[:]
 
 
@@ -91,9 +89,8 @@ def neighbor_joining(matrix, assoc_label):
         # Locate the coordinates with the lowest number in the matrix.
         x, y = calculate_distance(matrix, assoc_label)
 
-        # Update matrix after finding the lowest distance (cluster).
-        # After updating matrix, the labels are updated (joining the labels
-        # with the lowest distance).
+        # Creates a node of the chosen species and update the distance nj_matrix
+        # table. Deletes one label after joining two species.
         create_node_and_update_matrix(matrix, assoc_label, x, y)
 
     # Return the final label
@@ -104,9 +101,8 @@ def neighbor_joining(matrix, assoc_label):
 def main():
     # Association of each element of the matrix.
     assoc_label = ["GOR", "ORANG", "HUM", "CHI", "GIB"]
-    species_count = len(assoc_label)
-    # Building the matrix. This will be an triangular matrix. The other half
-    # is symmetric and the main diagonal is filled with zeros.
+
+    # Building the matrix.
     nj_matrix = [
         [0, 0.1890, 0.1100, 0.1130, 0.2150],
         [0.1890, 0, 0.1790, 0.1920, 0.2110],
@@ -115,8 +111,6 @@ def main():
         [0.2150, 0.2110, 0.2050, 0.2140, 0]
         ]
 
-    ix, iy = calculate_distance(nj_matrix, assoc_label)
-    create_node_and_update_matrix(nj_matrix, assoc_label, ix, iy)
 
     # Printing results after Neighbor Joining Method.
     print neighbor_joining(nj_matrix, assoc_label)
