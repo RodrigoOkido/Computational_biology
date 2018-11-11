a54b0474eb04ec58f6224d6ca58d033a7d53d198
RodrigoOkido <rodrigookido@gmail.com>
2018-11-11 21:01:42 -0200

    K-means algorithm refinements...

--- E8/e8-1a.py
+++ E8/e8-1a.py
@@ -1,7 +1,5 @@
-import math
 import random
 import numpy as np
-import os
 
 # Definition of clusters numbers (K param)
 clusters = 0
@@ -30,35 +28,42 @@ def define_point_to_cluster(distance, data_point, centroids):
     index_of_minimum = min(distance, key=distance.get)
     return [index_of_minimum, data_point, centroids[index_of_minimum]]
 
-def compute_new_centroids(cluster_label, centroids):
+
+# Function to modify the centroids.
+def new_centroids(cluster_label, centroids):
     return np.array(cluster_label + centroids)/2
 
 
+# Main function k-means.
 def k_means(data, centroids, total_iteration):
     label = []
-    cluster_label = []
+    cluster_label = [[] for i in range(len(centroids))]
+
     total_points = len(data)
     k = len(centroids)
-
     for iteration in range(0, total_iteration):
         for index_point in range(0, total_points):
             distance = {}
+            # Calculate the distance of each point between all centroids.
             for index_centroid in range(0, k):
-                distance[index_centroid] = euclidian_distance(data[index_point], centroids[index_centroid])
-            label = define_point_to_cluster(distance, data[index_point], centroids)
-            centroids[label[0]] = compute_new_centroids(label[1], centroids[label[0]])
+                distance[index_centroid] = euclidian_distance(data[index_point][iteration], centroids[index_centroid])
 
-            if iteration == (total_iteration - 1):
-                cluster_label.append(label)
+            label = define_point_to_cluster(distance, data[index_point][iteration], centroids)
+            cluster_label[label[0]].append(label[1])
+            centroids[label[0]] = new_centroids(label[1], centroids[label[0]])
 
     return [cluster_label, centroids]
+
 
 if __name__ == "__main__":
     with open('leukemia_big.csv', 'rb') as filename:
         next(filename)
         data = np.genfromtxt(filename, delimiter=",")
-        print data
+        # print data
         centroids = generate_centroids(3)
         total_iteration = 5
 
         [cluster_label, new_centroids] = k_means(data, centroids, total_iteration)
+
+        for i in range(len(centroids)):
+            print "Cluster ", i , ":", len(cluster_label[i])
